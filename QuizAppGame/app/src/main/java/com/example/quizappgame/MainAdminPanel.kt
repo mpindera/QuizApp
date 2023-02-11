ce5b8eed292d543ba687f03f059b3ac25c679f4c
mpindera <mikolaj.pindera@op.pl>
2023-02-11 16:39:39 +0100

    Add RecyclerLayout

--- QuizAppGame/app/src/main/java/com/example/quizappgame/MainAdminPanel.kt
+++ QuizAppGame/app/src/main/java/com/example/quizappgame/MainAdminPanel.kt
@@ -9,12 +9,12 @@ import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.UUID
 
 class MainAdminPanel : AppCompatActivity() {
     lateinit var recycler: RecyclerView
-    var textList = mutableListOf<QuizModel>()
     private lateinit var quizName: EditText
     private lateinit var addToQuiz: Button
     private lateinit var chackedVisible: CheckBox
@@ -27,6 +27,8 @@ class MainAdminPanel : AppCompatActivity() {
         chackedVisible = findViewById(R.id.chackedVisible)
         recycler = findViewById(R.id.quizRecycler)
 
+        val todoList = mutableListOf(QuizModel("1", "abc2", true, 1))
+        val adapter = QuizAdapter(todoList)
 
         addToQuiz.setOnClickListener {
             val id = UUID.randomUUID().toString()
@@ -34,27 +36,48 @@ class MainAdminPanel : AppCompatActivity() {
             val visible = chackedVisible.isChecked
 
             val quizModel = QuizModel(id, name, visible, 0)
+
+            todoList.add(quizModel)
+            adapter.notifyItemInserted(todoList.size - 1)
             uploadQuiz(quizModel)
+            recycler.adapter = adapter
+            recycler.layoutManager = LinearLayoutManager(this)
+            loadQuiz()
         }
-        recycler.layoutManager = LinearLayoutManager(applicationContext)
-        recycler.adapter = QuizAdapter(textList)
+
+    }
+
+    private fun loadQuiz() {
+        FirebaseFirestore.getInstance().collection("quiz").get()
+            .addOnSuccessListener { querySnapshot ->
+                val quizList = mutableListOf<QuizModel>()
+                for (document in querySnapshot) {
+                    val quiz = document.toObject(QuizModel::class.java)
+                    quizList.add(quiz)
+                }
+                val adapter = QuizAdapter(quizList)
+                recycler.adapter = adapter
+                recycler.layoutManager = LinearLayoutManager(this)
+            }
+            .addOnFailureListener { exception ->
+                
+            }
     }
 
     private fun uploadQuiz(quizModel: QuizModel) {
         FirebaseFirestore.getInstance().collection("quiz").document(quizModel.getQuizID())
             .set(quizModel)
         quizName.text.clear()
+
     }
 
     override fun onResume() {
         super.onResume()
-
+        loadQuiz()
         if (FirebaseAuth.getInstance().currentUser == null) {
             FirebaseAuth.getInstance().signInAnonymously()
                 .addOnSuccessListener {
                     Log.d("ad", "Works")
-
-
                 }
                 .addOnFailureListener {
                     Log.d("ad", "bad")
--- QuizAppGame/app/src/main/java/com/example/quizappgame/QuizAdapter.kt
+++ QuizAppGame/app/src/main/java/com/example/quizappgame/QuizAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.quizappgame
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,41 +9,31 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
-class QuizAdapter(private val items: List<QuizModel>) :
-    RecyclerView.Adapter<QuizAdapter.ViewHolder>() {
+class QuizAdapter(
+    private var items: List<QuizModel>
+) : RecyclerView.Adapter<QuizAdapter.ViewHolder>() {
 
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.admin_panel_test, parent, false)
-        return ViewHolder(v)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.admin_panel_test, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val quizModel = items[position]
-        holder.bindView(quizModel)
-    }
-
-    override fun getItemCount() = items.size
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-
-        private val addToQuiz: Button
-        private val quizName: EditText
-        private val chackedVisible: CheckBox
-        private val quizName2: TextView
-
-        init {
-            addToQuiz = itemView.findViewById(R.id.addToQuiz)
-            quizName = itemView.findViewById(R.id.quizName)
-            chackedVisible = itemView.findViewById(R.id.chackedVisible)
-            quizName2 = itemView.findViewById(R.id.quizName2)
-        }
-
-        fun bindView(quizModel: QuizModel) {
-            quizName2.text = quizModel.getQuizName()
-        }
+        val quizName2 = holder.itemView.findViewById<TextView>(R.id.quizName2)
+        quizName2.text = items[position].getQuizName()
+        val chackedVisible = holder.itemView.findViewById<CheckBox>(R.id.chackedVisible)
+        chackedVisible.isChecked = items[position].getVisible()
     }
 }
+
+
